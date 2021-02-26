427a043eaf1bbac6ff95a3879708bdf5b727daff
Raushan <raushan.043.kumar@gmail.com>
2021-02-26 14:37:16 +0530

    added link

--- src/About.jsx
+++ src/About.jsx
@@ -17,13 +17,21 @@ function About() {
                         <div className="col-lg-6 d-flex justify-content-center  flex-column align-items-center order-lg-1  cont  order-2">
                             <div className="upper text-black-50   ">
                                 <h2>
-                                    I am a CSE sophomore at IIEST,Shibpur.I am a full stack web developer and currently learning app development using Flutter.
+                                    I am a CSE sophomore at IIEST,Shibpur.I am a full stack web developer and currently learning Competitive programming using cpp.
                         </h2>
                             </div>
                             <div className="lower  text-black-50">
                                 <h4>
-                                    I am a curious person who enjoys figuring out the building blocks of the world, and rearranging them to build something even better. I always love to explore and keep learning new things from everyone.In my free time i read journals and articles about space discoveries and missions.I am highly inspired by John Bresina,Computer Scientist at NASA.
-                         </h4>
+                                    I am a curious person who enjoys figuring out the building blocks of the world, and rearranging them to build something even better. I always love to explore and keep learning new things from everyone.In my free time i read journals and articles about space discoveries and missions.
+                                 </h4>
+                                 <h4>
+                                     <span className=" text-black-50 display-8">
+                                         EMAIL :
+                                     </span>
+                                     <span className=" text-success mx-2">
+                                         raushan.043.kumar@gmail.com
+                                     </span>
+                                 </h4>
                             </div>
                         </div>
                         {/* <hr className="line" /> */}
@@ -31,7 +39,7 @@ function About() {
                             <div className="content  d-flex justify-content-center align-items-center flex-column  ">
 
                                 <div className="pic mt-5">
-                                {/* <div className="round ">
+                                    {/* <div className="round ">
 
                                     </div> */}
                                     <img className="img mt-5" alt="Remy Sharp" src="https://scontent.fixr3-1.fna.fbcdn.net/v/t1.0-9/96128996_712464202892001_551431297810038784_o.jpg?_nc_cat=102&ccb=3&_nc_sid=8bfeb9&_nc_ohc=x3loHkaM3dgAX9PRMuL&_nc_ht=scontent.fixr3-1.fna&oh=55fcf51b86a0c551d61ba298791d56ab&oe=605D5CDC" />
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,6 +5,7 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import EmailIcon from '@material-ui/icons/Email';
+import TwitterIcon from '@material-ui/icons/Twitter';
 
 function sendEmail(){
   var str = window.open('mailto:akshu2353@gmail.com', '_blank');
@@ -23,22 +24,22 @@ export default function LabelBottomNavigation() {
   return (
     <div className="container-fluid  footer d-flex justify-content-center align-items-center">
       <div id="foo" className="d-flex justify-content-center align-items-center" >
-        <a href="https://www.facebook.com/profile.php?id=100013267181447" target="_blank">
+        <a href="https://www.facebook.com/profile.php?id=100023852830406" target="_blank">
           <FacebookIcon style={{ fill: "blue", fontSize: "30px" }} />
         </a>
-        <a href="https://www.linkedin.com/in/akshat-agarwal-77a116188/" target="_blank">
+        <a href="https://www.linkedin.com/in/raushankumar43/" target="_blank">
           <LinkedInIcon style={{ fill: "#0e76a8 ", fontSize: "30px" }}  />
         </a>
-        <a href="https://www.instagram.com/itsaksht/" target="_blank">
+        <a href="https://www.instagram.com/tejas_r_28/" target="_blank">
         <InstagramIcon style={{ fill: "#8a3ab9", fontSize: "30px" }}  />
         </a>
-        <a href="https://github.com/akshat7593" target="_blank">
+        <a href="https://github.com/Tejas-r281" target="_blank">
           <GitHubIcon style={{ fill: "#F4CBB2", fontSize: "30px" }} />
         </a>
-         {/* <a href="mailto:akshu2353@gmail.com" target="_blank"> */}
-        <a style={{cursor:"pointer"}} onClick={sendEmail}>
-          <EmailIcon style={{ fill: "red", fontSize: "30px" }}  />
+        <a href="https://twitter.com/Raushan51745298" target="_blank">
+          <TwitterIcon style={{ fill: "#00acee", fontSize: "30px" }} />
         </a>
+
 </div>
     </div>
   );
